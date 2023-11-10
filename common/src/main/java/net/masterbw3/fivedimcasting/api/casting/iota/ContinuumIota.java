af538dabff3a8476f757b46b106e869079ad20a7
Master-Bw3 <bw1jochman@gmail.com>
2023-11-10 13:22:39 -0600

    Continuum Iota

---
+++ common/src/main/java/net/masterbw3/fivedimcasting/api/casting/iota/ContinuumIota.java
@@ -0,0 +1,61 @@
+package net.masterbw3.fivedimcasting.api.casting.iota;
+
+import at.petrak.hexcasting.api.casting.SpellList;
+import at.petrak.hexcasting.api.casting.iota.Iota;
+import at.petrak.hexcasting.api.casting.iota.IotaType;
+import at.petrak.hexcasting.api.utils.HexUtils;
+import net.masterbw3.fivedimcasting.lib.hex.FiveDimCastingIotaTypes;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ContinuumIota extends Iota {
+    private record Payload(Iota frontVal, SpellList genNextFunc, List<SpellList> maps) {
+    }
+
+    public ContinuumIota(Iota frontVal, SpellList genNextFunc, List<SpellList> maps) {
+        super(FiveDimCastingIotaTypes.CONTINUUM, new Payload(frontVal, genNextFunc, maps));
+    }
+
+    @Override
+    public boolean isTruthy() {
+        return true;
+    }
+
+    @Override
+    protected boolean toleratesOther(Iota iota) {
+        // TODO: check if same or something idk
+        return false;
+    }
+
+    @Override
+    public @NotNull NbtElement serialize() {
+        //TODO: serialize
+        return null;
+    }
+
+    public static IotaType<ContinuumIota> TYPE = new IotaType<>() {
+        @Override
+        public ContinuumIota deserialize(NbtElement tag, ServerWorld world) throws IllegalArgumentException {
+
+            //return new ContinuumIota();
+            return null;
+        }
+
+        @Override
+        public Text display(NbtElement nbtElement) {
+            return null;
+        }
+
+        @Override
+        public int color() {
+            return 0;
+        }
+
+
+    };
+}
---
+++ common/src/main/java/net/masterbw3/fivedimcasting/lib/hex/FiveDimCastingIotaTypes.kt
@@ -0,0 +1,32 @@
+package net.masterbw3.fivedimcasting.lib.hex
+
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.IotaType
+import net.masterbw3.fivedimcasting.api.FiveDimCastingApi.modLoc
+import net.masterbw3.fivedimcasting.api.casting.iota.ContinuumIota
+import net.minecraft.util.Identifier
+import org.jetbrains.annotations.ApiStatus
+import java.util.function.BiConsumer
+
+object FiveDimCastingIotaTypes {
+    @JvmStatic
+    @ApiStatus.Internal
+    fun registerTypes(r: BiConsumer<IotaType<*>, Identifier>) {
+        for ((key, value) in TYPES) {
+            r.accept(value, key)
+        }
+    }
+
+    private val TYPES: MutableMap<Identifier, IotaType<*>> = LinkedHashMap()
+
+    @JvmField
+    val CONTINUUM: IotaType<ContinuumIota> = type("continuum", ContinuumIota.TYPE)
+
+
+    private fun <U : Iota, T : IotaType<U>> type(name: String, type: T): T {
+        val old = TYPES.put(modLoc(name), type)
+        require(old == null) { "Typo? Duplicate id $name" }
+        return type
+    }
+
+}
