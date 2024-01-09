e4840490712a988442afb04833e5ce113285eb42
Master-Bw3 <bw1jochman@gmail.com>
2024-01-09 13:33:57 -0600

    grand staff

---
+++ common/src/main/java/net/masterbw3/fivedimcasting/common/items/ItemGrandStaff.java
@@ -0,0 +1,50 @@
+package net.masterbw3.fivedimcasting.common.items;
+
+import at.petrak.hexcasting.common.lib.HexSounds;
+import at.petrak.hexcasting.common.msgs.MsgClearSpiralPatternsS2C;
+import at.petrak.hexcasting.common.msgs.MsgOpenSpellGuiS2C;
+import at.petrak.hexcasting.xplat.IXplatAbstractions;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class ItemGrandStaff extends Item {
+
+    public ItemGrandStaff(Settings pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        if (player.isSneaking()) {
+            if (world.isClient()) {
+                player.playSound(HexSounds.STAFF_RESET, 1f, 1f);
+            } else if (player instanceof ServerPlayerEntity serverPlayer) {
+                IXplatAbstractions.INSTANCE.clearCastingData(serverPlayer);
+                var packet = new MsgClearSpiralPatternsS2C(player.getUuid());
+                IXplatAbstractions.INSTANCE.sendPacketToPlayer(serverPlayer, packet);
+                IXplatAbstractions.INSTANCE.sendPacketTracking(serverPlayer, packet);
+            }
+        }
+
+        if (!world.isClient() && player instanceof ServerPlayerEntity serverPlayer) {
+            var harness = IXplatAbstractions.INSTANCE.getStaffcastVM(serverPlayer, hand);
+            var patterns = IXplatAbstractions.INSTANCE.getPatternsSavedInUi(serverPlayer);
+            var descs = harness.generateDescs();
+
+            IXplatAbstractions.INSTANCE.sendPacketToPlayer(serverPlayer,
+                    new MsgOpenSpellGuiS2C(hand, patterns, descs.getFirst(), descs.getSecond(),
+                            0)); // TODO: Fix!
+        }
+
+        player.incrementStat(Stats.USED.getOrCreateStat(this));
+
+        return TypedActionResult.success(player.getStackInHand(hand));
+    }
+
+}
