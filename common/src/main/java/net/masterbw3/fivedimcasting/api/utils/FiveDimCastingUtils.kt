af4682bbb9253b1b69e4d4c0eb39b11d856bcabb
Master-Bw3 <bw1jochman@gmail.com>
2024-01-10 15:52:08 -0600

    hex outline and only allow drawing in hex

--- common/src/main/java/net/masterbw3/fivedimcasting/api/utils/FiveDimCastingUtils.kt
+++ common/src/main/java/net/masterbw3/fivedimcasting/api/utils/FiveDimCastingUtils.kt
@@ -2,9 +2,29 @@ package net.masterbw3.fivedimcasting.api.utils
 
 import at.petrak.hexcasting.api.casting.math.HexCoord
 import at.petrak.hexcasting.api.casting.math.HexPattern
+import at.petrak.hexcasting.api.utils.SQRT_3
+import at.petrak.hexcasting.client.render.drawLineSeq
 import net.minecraft.util.math.Vec2f
+import org.joml.Matrix4f
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
+import kotlin.math.sqrt
+
+
+fun drawSpotOutline(mat: Matrix4f, point: Vec2f, radius: Float, color: Int) {
+    val (x, y) = Pair(point.x, point.y)
+    val points = listOf(
+        Vec2f(x - 1f, y),
+        Vec2f(x - SQRT_3/4, y - SQRT_3/2),
+        Vec2f(x + SQRT_3/4, y - SQRT_3/2),
+        Vec2f(x + 1f, y),
+        Vec2f(x + SQRT_3/4, y + SQRT_3/2),
+        Vec2f(x - SQRT_3/4, y + SQRT_3/2),
+        )
+    drawLineSeq(mat, points.map { it.add(point).multiply(radius) }, 0.5f, 0f, color, color)
+
+}
+
 
 fun squareCoordToPx(coord: HexCoord, size: Float, offset: Vec2f): Vec2f {
     val x = (3.0/2.0 * coord.q.toFloat());
@@ -27,6 +47,6 @@ fun pxToSquareCoord(px: Vec2f, size: Float, offset: Vec2f): HexCoord {
         HexCoord(q, r + (rf + 0.5f * qf).roundToInt())
 }
 
-fun hexPatternToFlatCoordLines(pattern: HexPattern, hexSize: Float, origin: Vec2f): List<Vec2f> =
+fun hexPatternToSquareCoordLines(pattern: HexPattern, hexSize: Float, origin: Vec2f): List<Vec2f> =
     pattern.positions().map { squareCoordToPx(it, hexSize, origin) }
 
--- fabric/src/main/java/net/masterbw3/fivedimcasting/fabric/mixin/MixinGuiSpellCasting.java
+++ fabric/src/main/java/net/masterbw3/fivedimcasting/fabric/mixin/MixinGuiSpellCasting.java
@@ -2,8 +2,10 @@ package net.masterbw3.fivedimcasting.fabric.mixin;
 
 import at.petrak.hexcasting.api.casting.math.HexCoord;
 import at.petrak.hexcasting.client.gui.GuiSpellcasting;
+import at.petrak.hexcasting.client.render.RenderLib;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.masterbw3.fivedimcasting.FiveDimCasting;
 import net.masterbw3.fivedimcasting.mixinImpl.IMixinGuiSpellCasting;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -15,6 +17,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static at.petrak.hexcasting.client.render.RenderLib.screenCol;
 
 @Mixin(GuiSpellcasting.class)
 public abstract class MixinGuiSpellCasting extends Screen implements IMixinGuiSpellCasting {
@@ -36,8 +43,9 @@ public abstract class MixinGuiSpellCasting extends Screen implements IMixinGuiSp
         return fivedimcasting$grandStaffCasting;
     }
 
+    //set mouse coord to be center of screen
     @ModifyVariable(method = "render", at = @At("STORE"), ordinal = 0)
-    public HexCoord modifyMousePos(HexCoord mouseCoord) {
+    public HexCoord modifyMouseCoord(HexCoord mouseCoord) {
         if (fivedimcasting$isGrandStaffCasting()) {
             var $this = ((GuiSpellcasting)(Object)this);
             return $this.pxToCoord(new Vec2f((float) $this.width / 2, (float) $this.height / 2));
@@ -46,6 +54,7 @@ public abstract class MixinGuiSpellCasting extends Screen implements IMixinGuiSp
         }
     }
 
+    //offset by center instead of mouse coords
     @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec2f;add(Lnet/minecraft/util/math/Vec2f;)Lnet/minecraft/util/math/Vec2f;"))
     private Vec2f test(Vec2f instance, Vec2f vec, Operation<Vec2f> original) {
         if (fivedimcasting$isGrandStaffCasting()) {
@@ -57,6 +66,7 @@ public abstract class MixinGuiSpellCasting extends Screen implements IMixinGuiSp
         }
     }
 
+    //change radius
     @ModifyVariable(method = "render", at = @At("STORE"), ordinal = 2)
     public int modifyRadius(int radius) {
         if (fivedimcasting$isGrandStaffCasting()) {
@@ -66,20 +76,105 @@ public abstract class MixinGuiSpellCasting extends Screen implements IMixinGuiSp
         }
     }
 
-    @Inject(method = "render", at = @At("HEAD"))
-    public void mixinRenderHead(DrawContext graphics, int pMouseX, int pMouseY, float pPartialTick, CallbackInfo ci) {
-        var $this = ((GuiSpellcasting) (Object) this);
+    //make all visible dots with a size > 0.1 have a size of 1
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(FFF)F"))
+    private float modifyDotSize(float value, float min, float max, Operation<Float> original) {
         if (fivedimcasting$isGrandStaffCasting()) {
-
+            return (float) Math.ceil(original.call(value, min, max) - 0.1);
+        } else {
+            return original.call(value, min, max);
         }
     }
 
-    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(FFF)F"))
-    private float test(float value, float min, float max, Operation<Float> original) {
+//    //make unused dots outlined with no fill
+//    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/client/render/RenderLib;drawSpot(Lorg/joml/Matrix4f;Lnet/minecraft/util/math/Vec2f;FFFFF)V"))
+//    private void modifyDotStyle(Matrix4f mat, Vec2f point, float radius, float r, float g, float b, float a, Operation<Void> original) {
+//        if (fivedimcasting$isGrandStaffCasting()) {
+//            RenderSystem.defaultBlendFunc();
+//            FiveDimCastingUtilsKt.drawSpotOutline(mat, point, radius, 0xFFFECBE6);
+//        } else {
+//            original.call(mat, point, radius, r, g, b, a);
+//        }
+//    }
+
+    @WrapOperation(method = "mouseClicked", at = @At(value = "INVOKE", target = "Ljava/util/Set;contains(Ljava/lang/Object;)Z"))
+    private boolean mixinValidPointClicked(Set<HexCoord> usedSpots, Object coord, Operation<Boolean> original) {
         if (fivedimcasting$isGrandStaffCasting()) {
-            return (float) Math.ceil(original.call(value, min, max));
+            var $this = ((GuiSpellcasting) (Object) this);
+            var center = new Vec2f((float) $this.width / 2, (float) $this.height / 2).negate();
+            var delta = $this.coordToPx((HexCoord) coord).add(center).length();
+            var pointIsRendered = Math.ceil(Math.min(1f, (1.0f - ((delta - $this.hexSize()) / (5f * $this.hexSize())) - 0.1))) == 1;
+            return original.call(usedSpots, coord) || !pointIsRendered;
         } else {
-            return original.call(value, min, max);
+            return original.call(usedSpots, coord);
+        }
+    }
+
+    @WrapOperation(method = "mouseDragged", at = @At(value = "INVOKE", target = "Ljava/util/Set;contains(Ljava/lang/Object;)Z"))
+    private boolean mixinValidPointDragged(Set<HexCoord> usedSpots, Object coord, Operation<Boolean> original) {
+        if (fivedimcasting$isGrandStaffCasting()) {
+            var $this = ((GuiSpellcasting) (Object) this);
+            var center = new Vec2f((float) $this.width / 2, (float) $this.height / 2).negate();
+            var delta = $this.coordToPx((HexCoord) coord).add(center).length();
+            var pointIsRendered = Math.ceil(Math.min(1f, (1.0f - ((delta - $this.hexSize()) / (5f * $this.hexSize())) - 0.1))) == 1;
+            FiveDimCasting.LOGGER.info(pointIsRendered);
+            return original.call(usedSpots, coord) || !pointIsRendered;
+        } else {
+            return original.call(usedSpots, coord);
+        }
+    }
+
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;defaultBlendFunc()V", shift = At.Shift.AFTER))
+    public void mixinRenderDrawLines(DrawContext graphics, int pMouseX, int pMouseY, float pPartialTick, CallbackInfo ci) {
+        if (fivedimcasting$isGrandStaffCasting()) {
+            var $this = ((GuiSpellcasting) (Object) this);
+            var mat = graphics.getMatrices().peek().getPositionMatrix();
+
+            //settings
+            final float line_width = 1;
+            final int color = screenCol(0xff_64c8ff);
+            float y_offset = (float) (5 * $this.width) / $this.height;
+            float x_offset = (float) (y_offset * (2.0 / Math.sqrt(3.0)) / 2) ;
+
+            //center hexagon dimensions
+            var hex_top_y = $this.coordToPx(new HexCoord(0, -3)).y;
+            var hex_top_left_x = $this.coordToPx(new HexCoord(0, -3)).x;
+            var hex_top_right_x = $this.coordToPx(new HexCoord(3, -3)).x;
+
+            var hex_middle_y = $this.coordToPx(new HexCoord(0, 0)).y;
+            var hex_middle_left_x = $this.coordToPx(new HexCoord(-3, 0)).x;
+            var hex_middle_right_x = $this.coordToPx(new HexCoord(3, 0)).x;
+
+
+            var hex_bottom_y = $this.coordToPx(new HexCoord(0, 3)).y;
+            var hex_bottom_left_x = $this.coordToPx(new HexCoord(-3, 3)).x;
+            var hex_bottom_right_x = $this.coordToPx(new HexCoord(0, 3)).x;
+
+            //draw bottom center hexagon outline
+            var bottomCenterOutlinePoints = new ArrayList<Vec2f>();
+            var bottom_left = new Vec2f(hex_bottom_left_x - x_offset, hex_bottom_y + y_offset);
+            var bottom_right = new Vec2f(hex_bottom_right_x + x_offset, hex_bottom_y + y_offset);
+            var middle_right = new Vec2f(hex_middle_right_x + x_offset * 2, hex_middle_y);
+
+            bottomCenterOutlinePoints.add(bottom_left);
+            bottomCenterOutlinePoints.add(bottom_right);
+            bottomCenterOutlinePoints.add(middle_right);
+
+            RenderLib.drawLineSeq(mat, bottomCenterOutlinePoints, line_width,0, color, color);
+
+            //draw top center hexagon outline
+            //draw bottom center hexagon outline
+            var topCenterOutlinePoints = new ArrayList<Vec2f>();
+            var top_right = new Vec2f(hex_top_right_x + x_offset, hex_top_y - y_offset);
+            var top_left = new Vec2f(hex_top_left_x - x_offset, hex_top_y - y_offset);
+            var middle_left = new Vec2f(hex_middle_left_x - x_offset * 2, hex_middle_y);
+
+            topCenterOutlinePoints.add(top_right);
+            topCenterOutlinePoints.add(top_left);
+            topCenterOutlinePoints.add(middle_left);
+
+            RenderLib.drawLineSeq(mat, topCenterOutlinePoints, line_width,0, color, color);
         }
     }
 
